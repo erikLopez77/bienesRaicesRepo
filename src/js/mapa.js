c28fb2b4b65266cedab6f0c6b8462ce9b86a73f4
ErikLopez <espinozalopezerik@gmail.com>
2025-01-31 12:43:01 -0600

    Se agregó pin, campos ocultos que guardan calle, latlng, se muestra la calle

--- src/js/mapa.js
+++ src/js/mapa.js
@@ -3,11 +3,36 @@
     const lat = 19.001634;
     const lng = -98.2044694;
     const mapa = L.map('mapa').setView([lat, lng], 12);
+    let marker;
 
+    //usar Provider y Geocoder
+    const geocodeService=L.esri.Geocoding.geocodeService();
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(mapa);
+    //pin
+    marker = new L.marker([lat,lng],{
+        draggable: true,//p mover el pin
+        autoPan: true//centra el mapa
+    }).addTo(mapa);
 
+    //latitud y longitud
+    marker.on('moveend', function(evt){
+        marker=evt.target;
+        const posicion=marker.getLatLng();
+        mapa.panTo(new L.getLatLng(posicion.lat,posicion.lng));
+
+        //obtener calle al soltar el pin
+        geocodeService.reverse().latlng(posicion,13).run(function(error,resultado){
+            marker.bindPopup(resultado.address.LongLabel);
+            //llenar los campos
+            document.querySelector('.calle').textContent=resultado?.address?.Address ?? '';
+            document.querySelector('#calle').value=resultado?.address?.Address ?? '';
+            document.querySelector('#lat').value=resultado?.latlng?.lat ?? '';
+            document.querySelector('#lng').value=resultado?.latlng?.lng ?? '';
+
+        });
+    });
 
 })()
