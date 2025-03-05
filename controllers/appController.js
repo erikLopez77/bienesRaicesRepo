a3b179c8e767dfa01e515e947d0865d4a4aa37a3
ErikLopez <espinozalopezerik@gmail.com>
2025-03-04 22:43:07 -0600

    falta filtar por precio, falta revisar implementación de los filtros

--- controllers/appController.js
+++ controllers/appController.js
@@ -1,5 +1,10 @@
-const inicio = (req, res) => {
-    res.render('inicio', { pagina: 'Inicio' });
+import { Precio, Categoria, Propiedad } from '../models/index.js';
+const inicio = async (req, res) => {
+    const [categorias, precios] = await Promise.all([
+        Categoria.findAll({ raw: true }),
+        Precio.findAll({ raw: true }),
+    ])
+    res.render('inicio', { pagina: 'Inicio', categorias, precios });
 }
 
 const categoria = (req, res) => {
