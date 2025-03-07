18cc2b061751f52b556a81bdee24c60a9b670f9e
ErikLopez <espinozalopezerik@gmail.com>
2025-03-07 14:05:00 -0600

    Ya se cambia el colory en db al cambiar de edo., bug en mostrar texto de publicado

--- src/js/cambiarEstado.js
+++ src/js/cambiarEstado.js
@@ -19,6 +19,18 @@
             });
             const resultado = await respuesta.json();
             console.log(resultado);
+            if (resultado) {
+                if (e.target.classList.contains('bg-yellow-100')) {
+                    e.target.classList.remove('bg-yellow-100', 'text-yellow-800')
+                    e.target.classList.add('bg-green-100', 'text-green-800')
+                    e.target.textContent = 'Publicado'
+
+                } else {
+                    e.target.classList.remove('bg-green-100', 'text-green-800')
+                    e.target.classList.add('bg-yellow-100', 'text-yellow-800')
+                    e.target.textContent = 'No publicado'
+                }
+            }
 
         } catch (error) {
             console.log(error);
